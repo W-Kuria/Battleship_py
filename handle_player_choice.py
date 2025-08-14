d34e03fcfaa79ce8f1e29232af3ca99e1142cef6
W-Kuria <william.njoroge.kuria@gmail.com>
2025-08-14 15:26:18 +0300

    feat: Add conditions

---
+++ handle_player_choice.py
@@ -0,0 +1,14 @@
+
+
+# 3: Match the choice to the character
+if char_choice == "1":
+    character = "Wizard"
+elif char_choice == "2":
+    character = "Elf"
+elif char_choice == "3":
+    character = "Human"
+else:
+    character = "Unknown"
+
+# 4: Confirm the selection
+print(f"You have chosen: {character}")
