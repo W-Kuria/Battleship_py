925a6b486356484b61b16371a215fd1e5dbbec53
W-Kuria <william.njoroge.kuria@gmail.com>
2025-08-14 15:21:51 +0300

    feat: Minor polish

--- Player_choice.py
+++ Player_choice.py
@@ -7,18 +7,7 @@ print("2) Elf")
 print("3) Human")
 
 # 2: Get the player's choice
-choice = input("Enter the number of your choice: ")
+char_choice = input("Enter the number of your choice: ")
 
 
-# 3: Match the choice to the character
-if choice == "1":
-    character = "Wizard"
-elif choice == "2":
-    character = "Elf"
-elif choice == "3":
-    character = "Human"
-else:
-    character = "Unknown"
 
-# 4: Confirm the selection
-print(f"You have chosen: {character}")
