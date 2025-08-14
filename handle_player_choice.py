725f414b70edf0247cc6cc14f4bd207f8cf5fab1
W-Kuria <william.njoroge.kuria@gmail.com>
2025-08-14 15:32:33 +0300

    feat: Reassigning of variables

--- handle_player_choice.py
+++ handle_player_choice.py
@@ -1,12 +1,21 @@
-
+char_choice = input("Enter the number of your choice: ")
 
 # 3: Match the choice to the character
 if char_choice == "1":
     character = "Wizard"
+    my_hp = wizard_hp
+    my_damage = wizard_damage
+    
 elif char_choice == "2":
     character = "Elf"
+    my_hp = elf_hp
+    my_damage = elf_damage
+    
 elif char_choice == "3":
     character = "Human"
+    my_hp = human_hp
+    my_damage = human_damage
+    
 else:
     character = "Unknown"
 
